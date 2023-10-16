74934e5096adb0e67c9e0fe7ee7e2416c4403e10
hvsk004 <himanth2004@gmail.com>
2023-10-16 19:38:21 +0530

    Max Element2 in a Binary Tree

--- BinaryTrees/maxNumber.cpp
+++ BinaryTrees/maxNumber.cpp
@@ -9,11 +9,28 @@ public:
     Node *right;
     Node(int value) : key(value), left(NULL), right(NULL) {}
 };
-int maxElement(Node *root)
+int maxElement(Node *root) // Recursive approach best when height is less and width is more.
 {
     if (root == NULL)
-        return 0;
+        return INT_MIN;
     return max(root->key, max(maxElement(root->right), maxElement(root->left)));
+}
+int maxElement2(Node *root) // Iterative approach best for trees with large height since there will be no recursion overhead
+{
+    int maxValue = 0;
+    queue<Node *> q;
+    q.push(root);
+    while (!q.empty())
+    {
+        Node *curr = q.front();
+        q.pop();
+        maxValue = max(maxValue, curr->key);
+        if (curr->left != NULL)
+            q.push(curr->left);
+        if (curr->right != NULL)
+            q.push(curr->right);
+    }
+    return maxValue;
 }
 int main()
 {
@@ -24,7 +41,7 @@ int main()
     tree->right->left = new Node(40);
     tree->right->right = new Node(50);
     tree->right->left->left = new Node(60);
-    tree->right->left->right = new Node(70);
-    cout << "The Max Element is: " << maxElement(tree);
+    tree->right->left->right = new Node(10);
+    cout << "The Max Element is: " << maxElement2(tree);
     return 0;
 }
