c36d0308bc0afa8d4f11d48182f95c12b71aaace
hvsk004 <himanth2004@gmail.com>
2023-10-18 17:12:45 +0530

    Recursive Palindrome

--- Recursion/checkPalindrome.cpp
+++ Recursion/checkPalindrome.cpp
@@ -1,24 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
-bool compare(char a, char b)
-{
-    if (a == b)
-        return true;
-    return false;
-}
-bool checkPalindrome(string str, int n, int i = 0)
+bool checkPalindrome2(string str, int n, int i = 0)
 {
     if (i >= n / 2)
         return true;
-    if (compare(str.at(i), str.at(n - i - 1)))
-        return checkPalindrome(str, n, i + 1);
+    if (str[i] == str[n - i - 1])
+        return checkPalindrome2(str, n, i + 1);
     return false;
 }
 int main()
 {
     string s1 = "Hello";
     string s2 = "SOS";
-    cout << "S1: " << checkPalindrome(s1, s1.length()) << endl;
-    cout << "S2: " << checkPalindrome(s2, s2.length()) << endl;
+    cout << "S1: " << checkPalindrome2(s1, s1.length()) << endl;
+    cout << "S2: " << checkPalindrome2(s2, s2.length()) << endl;
+    cout << "Enter a String : ";
+    string s;
+    cin >> s;
+    cout << "s: " << checkPalindrome2(s, s.length());
     return 0;
 }
