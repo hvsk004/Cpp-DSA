ab3e471c6349667f67467f831d3d485bf31d3b9a
hvsk004 <himanth2004@gmail.com>
2023-10-21 22:18:48 +0530

    Selection Sort

---
+++ sortingAlgorithms/selectionSort.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+void selectionSort(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        int minN = i;
+        for (int j = i; j < n; j++)
+        {
+            if (arr[minN] > arr[j])
+            {
+                minN = j;
+            }
+        }
+        int temp = arr[i];
+        arr[i] = arr[minN];
+        arr[minN] = temp;
+    }
+}
+int main()
+{
+    int arr[5] = {10, 40, 67, 22, 12};
+    selectionSort(arr, 5);
+    for (int i : arr)
+    {
+        cout << i << " ";
+    }
+    return 0;
+}
